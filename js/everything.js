99fa8cf1ecde062c3b99a0c255d9b0b3afdb95a3
Corrina Tang <corrinatang@hotmail.ca>
2020-07-09 02:33:49 -0700

    Further Javascripts

--- js/everything.js
+++ js/everything.js
@@ -83,7 +83,7 @@ console.log('JavaScript is running.');
       /*document.getElementById("overlay").style.display = "none";*/
     }
 
-      /* When the user clicks on the button,
+    /* When the user clicks on the button,
   toggle between hiding and showing the dropdown content */
   function dropDownFunction() {
     document.getElementById("myDropdown").classList.toggle("show");
@@ -125,7 +125,59 @@ console.log('JavaScript is running.');
     document.getElementById("saucyBoySidenav").style.width = "0";
     /*document.getElementById("overlay").style.display = "none";*/
   }
+ /* When the user clicks on the button,
+    toggle between hiding and showing the dropdown content */
+    function dropDownFunction() {
+      document.getElementById("myDropdown").classList.toggle("show");
+    }
 
+    /* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
+    function myFunction() {
+      var x = document.getElementById("myLinks");
+      var y = document.getElementById("saucyBoySidenav");
+      if (x.style.display === "block") {
+        x.style.display = "none";
+
+      } else {
+        y.style.width="12rem";
+        /*document.getElementById("overlay").style.display = "block";*/
+      }
+    }
+
+    /* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
+
+    /* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
+    function closeNav() {
+      document.getElementById("saucyBoySidenav").style.width = "0";
+      /*document.getElementById("overlay").style.display = "none";*/
+    }
+
+     function up(max) {
+    document.getElementById("myNumber").value = parseInt(document.getElementById("myNumber").value) + 1;
+    if (document.getElementById("myNumber").value >= parseInt(max)) {
+      document.getElementById("myNumber").value = max;
+    }
+  }
+  function down(min) {
+    document.getElementById("myNumber").value = parseInt(document.getElementById("myNumber").value) - 1;
+    if (document.getElementById("myNumber").value <= parseInt(min)) {
+      document.getElementById("myNumber").value = min;
+    }
+  }
+
+  function up2(max) {
+    document.getElementById("myNumber2").value = parseInt(document.getElementById("myNumber2").value) + 1;
+    if (document.getElementById("myNumber2").value >= parseInt(max)) {
+      document.getElementById("myNumber2").value = max;
+    }
+  }
+  function down2(min) {
+    document.getElementById("myNumber2").value = parseInt(document.getElementById("myNumber2").value) - 1;
+    if (document.getElementById("myNumber2").value <= parseInt(min)) {
+      document.getElementById("myNumber2").value = min;
+    }
+  }
+  
 
 
 
