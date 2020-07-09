bb4d60f2fe2244d2e9de2f6c5cb05b32c3510eba
Corrina Tang <corrinatang@hotmail.ca>
2020-07-09 02:48:57 -0700

    Got rid of overlay tag

--- js/everything.js
+++ js/everything.js
@@ -179,5 +179,32 @@ console.log('JavaScript is running.');
   }
   
 
+    // Get the modal
+  var modal = document.getElementById("FreeShippingModal");
+
+  // Get the button that opens the modal
+  var link = document.getElementById("modalLink");
+
+  // Get the <span> element that closes the modal
+  var span = document.getElementsByClassName("close")[0];
+
+  // When the user clicks on the button, open the modal
+  link.onclick = function() {
+    modal.style.display = "block";
+  }
+
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function() {
+    modal.style.display = "none";
+  }
+
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
+
+
 
 
